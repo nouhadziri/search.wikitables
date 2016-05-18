53515e9eb8e2476d0d67ee38883b45acbee884d5
Denilson Barbosa <denilson@ualberta.ca>
2016-05-18 14:11:49 -0600

    class template for the wikipedia table reader.

---
+++ src/ca/ualberta/wikipedia/tablereader/WikipediaTableReader.java
@@ -0,0 +1,14 @@
+package ca.ualberta.wikipedia.tablereader;
+
+public class WikipediaTableReader {
+
+    public WikipediaTableReader() {
+	// TODO Auto-generated constructor stub
+    }
+
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+    }
+
+}
